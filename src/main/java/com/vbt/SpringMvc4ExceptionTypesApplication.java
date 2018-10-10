c28f8e9cac6e9029b3f3b18f15dabd8c7e7a3f67
Rajeshkumar Elumalai <38979122+elumalair@users.noreply.github.com>
2018-10-10 16:19:11 +0530

    SpringMVC4ExceptionTypes
    SpringMVC4ExceptionTypes

---
+++ src/main/java/com/vbt/SpringMvc4ExceptionTypesApplication.java
@@ -0,0 +1,12 @@
+package com.vbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc4ExceptionTypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvc4ExceptionTypesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vbt/SpringMvc4ExceptionTypesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vbt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvc4ExceptionTypesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
